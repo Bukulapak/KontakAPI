8d114393033a69c2970e229d81837d7cb3c7aa9a
Indra Riksa Herlambang <indrariksa@gmail.com>
2023-08-21 14:43:58 +0700

    tambah cors

--- config/cors.go
+++ config/cors.go
@@ -27,7 +27,7 @@ var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
 var Cors = cors.Config{
 	AllowOrigins:     strings.Join(origins[:], ","),
 	AllowMethods:     "GET,HEAD,OPTIONS,POST,PUT,DELETE",
-	AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+	AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, Token, Access-Control-Allow-Origin",
 	ExposeHeaders:    "Content-Length",
 	AllowCredentials: true,
 }
